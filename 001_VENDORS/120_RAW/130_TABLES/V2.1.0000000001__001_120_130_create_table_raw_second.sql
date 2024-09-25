5920a830598091b67a279ccaa8d6abc0da49676c
Neeraj Kumar <neeraj.s.kumar@kipi.bi>
2024-09-25 17:05:47 +0530

    3

---
+++ 001_VENDORS/120_RAW/130_TABLES/V2.1.0000000001__001_120_130_create_table_raw_second.sql
@@ -0,0 +1,13 @@
+USE DATABASE VENDORS;
+USE SCHEMA  RAW;
+CREATE TABLE raw_second
+(
+   FIRST_NAME VARCHAR
+  ,LAST_NAME VARCHAR
+);
+
+--tables 130
+--multiple databases issue(new dataabse added in case)
+--can we start database prefixing with 10s (001_vendors,002_peoplesoft)
+--for any object,prefixing should be constant for every database and table
+--Gap between objects
